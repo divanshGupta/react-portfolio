0febb9241e8f90bf0e70f25165ba0deb6871dd93
Divyansh Raj Gupta <42691376+divanshGupta@users.noreply.github.com>
2024-05-05 16:29:10 +0530

    update

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,19 +2,19 @@ export default [
     {
         year: '2023',
         title: 'React Developer',
-        duration: '6 Months',
+        duration: '+1 Year',
         discription: '',
     },
     {
-        year: '2023',
+        year: '2022',
         title: 'Front End Developer',
-        duration: '6 Months',
+        duration: '+2 Year',
         discription: '',
     },
     {
         year: '2022',
         title: 'Bachelor in Computer Applications',
-        duration: '3 Years',
+        duration: '3 Year',
         discription: '',
     },
 ]
