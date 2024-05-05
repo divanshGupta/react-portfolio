a4eb00c734a5dd71d8667b26904776b402f453cb
Divyansh Raj Gupta <42691376+divanshGupta@users.noreply.github.com>
2024-05-05 16:30:08 +0530

    push naa

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,9 @@ function Intro() {
     <div className="flex flex-col items-center justify-center text-center pt-20 pb-6">
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Divyansh <span className='text-amber-400'>Gupta</span></h1>
       <p className='text-base md:text-xl mb-3 font-medium'>Frontend Web Developer</p>
-      <p className='text-sm max-w-xl mb-6 font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, repellendus, quisquam soluta minima iure voluptatem, cum nihil ducimus sunt beatae ad tempore laudantium blanditiis voluptas possimus ut quia commodi facilis praesentium quis cupiditate. Commodi aliquid laborum ratione numquam quo culpa quos debitis illo sed iure, deleniti vitae atque unde ipsum?</p>
+      <p className='text-sm max-w-xl mb-6 font-bold'>Detail-oriented Front End Web Developer with a passion for crafting intuitive and visually appealing web
+experiences. Proficient in HTML, CSS, JavaScript, and various frameworks, dedicated to translating
+ideas into functional web applications.</p>
     </div>
   )
 }
